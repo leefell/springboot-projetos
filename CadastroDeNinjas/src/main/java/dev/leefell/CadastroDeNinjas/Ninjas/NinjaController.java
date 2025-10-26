0b9d462bc3112d9cfd4e6ebf3c201d1fd2259bf5
Alexandre <alexandreaug21@gmail.com>
2025-10-26 11:29:36 -0300

    fix: altera o endereco base das rotas do controller de ninja

--- CadastroDeNinjas/src/main/java/dev/leefell/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/leefell/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -4,7 +4,7 @@ import org.hibernate.annotations.SoftDelete;
 import org.springframework.web.bind.annotation.*;
 
 @RestController // Indica que esta classe é um controller REST (retorna dados, não páginas HTML)
-@RequestMapping // Define a URL base para os endpoints deste controller
+@RequestMapping("/ninjas") // Define a URL base para os endpoints deste controller
 public class NinjaController {
 
     @GetMapping("/boasvindas") // Mapeia requisições GET no caminho "/boasvindas"
@@ -20,25 +20,25 @@ public class NinjaController {
 
     // Mostrar (READ)
     @GetMapping("/todos")
-    public String buscarTodosOsNinjas(){
+    public String buscarTodosOsNinjas() {
         return "Todos os ninjas";
     }
 
     // Procura por ID (READ)
     @GetMapping("/ID")
-    public String buscarNinjaPorID(){
+    public String buscarNinjaPorID() {
         return "ninja com id tal";
     }
 
     // Alterar dados (UPDATE)
     @PutMapping("/ID")
-    public String alterarNinjaPorID(){
+    public String alterarNinjaPorID() {
         return "Ninja alterado";
     }
 
     // Deletar (DELETE)
     @DeleteMapping("/ID")
-    public String deletarNinjaPorID(){
+    public String deletarNinjaPorID() {
         return "Ninja deletado";
     }
 }
