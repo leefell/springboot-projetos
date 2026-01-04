51246a12530eb6a7cb75afeffa6aa84d04633a56
Alexandre <alexandreaug21@gmail.com>
2026-01-04 16:29:06 -0300

    fix: DTO agora usa Integer ao inves do tipo primitivo int para atualizacao PATCH funcionar

--- CadastroDeNinjas/src/main/java/dev/leefell/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ CadastroDeNinjas/src/main/java/dev/leefell/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -13,7 +13,7 @@ public class NinjaDTO {
     private Long id;
     private String nome;
     private String email;
-    private int idade;
+    private Integer idade;
     private MissoesModel missoes;
     private String rank;
 
