83106cab3e4e1282a1d6b7b76fdc0585d2c5fddc
Alexandre <alexandreaug21@gmail.com>
2026-01-18 18:54:31 -0300

    refactor: define a injecao de depencia como final e adiciona o thymeleaf

--- CadastroDeNinjas/src/main/java/dev/leefell/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/leefell/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequestMapping("/ninjas") // Define a URL base para os endpoints deste controller
 public class NinjaController {
 
-    private NinjaService ninjaService;
+    private final NinjaService ninjaService;
 
     public NinjaController(NinjaService ninjaService) {
         this.ninjaService = ninjaService;
